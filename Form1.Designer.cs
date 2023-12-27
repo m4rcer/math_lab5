d47f3a19ccea9a7fb7623dd9781f59b525af37b9
m4rcer <m4rcer@gmail.com>
2023-12-28 01:02:48 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace math_lab5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.elementHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart1 = new LiveCharts.Wpf.CartesianChart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.elementHost2 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart2 = new LiveCharts.Wpf.CartesianChart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.elementHost3 = new System.Windows.Forms.Integration.ElementHost();
+            this.cartesianChart3 = new LiveCharts.Wpf.CartesianChart();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // elementHost1
+            // 
+            this.elementHost1.Location = new System.Drawing.Point(43, 74);
+            this.elementHost1.Name = "elementHost1";
+            this.elementHost1.Size = new System.Drawing.Size(695, 418);
+            this.elementHost1.TabIndex = 0;
+            this.elementHost1.Text = "elementHost1";
+            this.elementHost1.Child = this.cartesianChart1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(435, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "N=10";
+            // 
+            // elementHost2
+            // 
+            this.elementHost2.Location = new System.Drawing.Point(796, 74);
+            this.elementHost2.Name = "elementHost2";
+            this.elementHost2.Size = new System.Drawing.Size(1070, 418);
+            this.elementHost2.TabIndex = 0;
+            this.elementHost2.Text = "elementHost1";
+            this.elementHost2.Child = this.cartesianChart2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1294, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "N=50";
+            // 
+            // elementHost3
+            // 
+            this.elementHost3.Location = new System.Drawing.Point(221, 558);
+            this.elementHost3.Name = "elementHost3";
+            this.elementHost3.Size = new System.Drawing.Size(1066, 379);
+            this.elementHost3.TabIndex = 0;
+            this.elementHost3.Text = "elementHost1";
+            this.elementHost3.Child = this.cartesianChart3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(682, 519);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "N=500";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.elementHost3);
+            this.Controls.Add(this.elementHost2);
+            this.Controls.Add(this.elementHost1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Integration.ElementHost elementHost1;
+        private LiveCharts.Wpf.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Integration.ElementHost elementHost2;
+        private LiveCharts.Wpf.CartesianChart cartesianChart2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Integration.ElementHost elementHost3;
+        private LiveCharts.Wpf.CartesianChart cartesianChart3;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,289 @@
+﻿using LiveCharts.Wpf;
+using LiveCharts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace math_lab5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            float x0 = 0;
+            float xn = 1f;
+            float y0 = 0;
+
+
+            var eilerPoints1 = Eiler(10, x0, xn, y0);
+            var eilerPoints2 = Eiler(50, x0, xn, y0);
+            var eilerPoints3 = Eiler(500, x0, xn, y0);
+
+            var rungeKuttPoints1 = RungeKutt(10, x0, xn, y0);
+            var rungeKuttPoints2 = RungeKutt(50, x0, xn, y0);
+            var rungeKuttPoints3 = RungeKutt(500, x0, xn, y0);
+
+            string format = "{00.00;-00.00}";
+
+
+            SeriesCollection series = new SeriesCollection(); //отображение данных на график. Линии и т.д.
+            ChartValues<double> zp = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in eilerPoints1) //Заполняем коллекции
+            {
+                try
+                {
+                    if (!double.IsInfinity(item.Item2))
+                    {
+                        zp.Add(item.Item2);
+                        date.Add(item.Item1);
+                    }
+                }
+                catch(Exception e)
+                {
+
+                }
+            }
+            cartesianChart1.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart1.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line.Title = "Y";
+            line.Values = zp;
+
+            ChartValues<double> zp2 = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date2 = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in rungeKuttPoints1) //Заполняем коллекции
+            {
+                try
+                {
+                    if (!double.IsInfinity(item.Item2))
+                    {
+                        zp2.Add(item.Item2);
+                        date2.Add(item.Item1);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            cartesianChart1.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart1.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date2.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line2 = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line2.Title = "Y";
+            line2.Values = zp2;
+
+            series.Add(line); //Добавляем линию на график
+            series.Add(line2); //Добавляем линию на график
+            cartesianChart1.Series = series; //Отрисовываем график в интерфейсе
+
+
+
+
+
+            SeriesCollection series2 = new SeriesCollection(); //отображение данных на график. Линии и т.д.
+            ChartValues<double> zp3 = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date3 = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in eilerPoints2) //Заполняем коллекции
+            {
+                try
+                {
+                    if (!double.IsInfinity(item.Item2))
+                    {
+                        zp3.Add(item.Item2);
+                        date3.Add(item.Item1);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            cartesianChart2.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart2.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date3.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line3 = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line3.Title = "Y";
+            line3.Values = zp3;
+
+            ChartValues<double> zp4 = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date4 = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in rungeKuttPoints2) //Заполняем коллекции
+            {
+                try
+                {
+                    if (!double.IsInfinity(item.Item2))
+                    {
+                        zp4.Add(item.Item2);
+                        date4.Add(item.Item1);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            cartesianChart2.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart2.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date4.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line4 = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line4.Title = "Y";
+            line4.Values = zp4;
+
+            series2.Add(line3); //Добавляем линию на график
+            series2.Add(line4); //Добавляем линию на график
+            cartesianChart2.Series = series2; //Отрисовываем график в интерфейсе
+
+
+
+
+
+            SeriesCollection series3 = new SeriesCollection(); //отображение данных на график. Линии и т.д.
+            ChartValues<double> zp5 = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date5 = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in eilerPoints3) //Заполняем коллекции
+            {
+                try
+                {
+                    if (!double.IsInfinity(item.Item2))
+                    {
+                        zp5.Add(item.Item2);
+                        date5.Add(item.Item1);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            cartesianChart3.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart3.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date3.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line5 = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line5.Title = "Y";
+            line5.Values = zp5;
+
+            ChartValues<double> zp6 = new ChartValues<double>(); //Значения которые будут на линии, будет создания чуть позже.
+            List<double> date6 = new List<double>(); //здесь будут храниться значения для оси X
+            foreach (var item in rungeKuttPoints3) //Заполняем коллекции
+            {
+                try
+                {
+                    if (!double.IsInfinity(item.Item2))
+                    {
+                        zp6.Add(item.Item2);
+                        date6.Add(item.Item1);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                }
+            }
+            cartesianChart3.AxisX.Clear(); //Очищаем ось X от значений по умолчанию
+            cartesianChart3.AxisX.Add(new Axis //Добавляем на ось X значения, через блок инициализатора.
+            {
+                Title = "X",
+                Labels = date6.Select(x => decimal.Round((decimal)x, 2).ToString()).ToArray()
+            });
+
+            LineSeries line6 = new LineSeries(); //Создаем линию, задаем ей значения из коллекции
+            line6.Title = "Y";
+            line6.Values = zp6;
+
+            series3.Add(line5); //Добавляем линию на график
+            series3.Add(line6); //Добавляем линию на график
+            cartesianChart3.Series = series3; //Отрисовываем график в интерфейсе
+        }
+
+
+        (double, double)[] Eiler(int n, float x0, float xn, float y0)
+        {
+            List<(double, double)> result = new List<(double, double)>();
+
+            double
+                 x = x0,
+                 y = y0;
+
+            var h = getH(n, x0, xn);
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                result.Add((x, y));
+                y = y + h * f(x, y);
+                x += h;
+            }
+
+            return result.ToArray();
+        }
+
+        (double, double)[] RungeKutt(int n, float x0, float xn, float y0)
+        {
+            List<(double, double)> result = new List<(double, double)>();
+
+            double
+                 x = x0,
+                 y = y0;
+
+            var h = getH(n, x0, xn);
+
+            for (int i = 0; i < n + 1; i++)
+            {
+                result.Add((x, y));
+                var k1 = h * f(x, y);
+                var k2 = h * f(x + h / 2, y + k1 / 2);
+                var k3 = h * f(x + h / 2, y + k2 / 2);
+                var k4 = h * f(x + h, y + k3);
+                y = y + (k1 + 2 * k2 + 2 * k3 + k4) / 6;
+                x += h;
+            }
+
+            return result.ToArray();
+        }
+
+
+        double f(double x, double y)
+        {
+            return Math.Pow(Math.E, y);
+        }
+
+        float getH(int n, float x0, float xn)
+        {
+            return (xn - x0) / n;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
